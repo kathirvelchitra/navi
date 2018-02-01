7501d8fc38b807e4818c4eb41629dcf5e00c11a8
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-01 08:00:49 +0000

    Create str.c

---
+++ str.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+int i,k;
+char str[20];
+printf("enter the word display no of times");
+scanf("%d",&k);
+printf("enter the word");
+scanf("%s",str);
+for(i=0;i<=k;i++)
+{
+printf("\n%s",str);
+}
+}
+
